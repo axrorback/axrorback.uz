021d6b49408650e2746ede1078a769d19d03e85b
axrorback <axacyber1@gmail.com>
2025-12-25 21:48:04 +0500

    db prepare for heroku

--- config/settings.py
+++ config/settings.py
@@ -89,10 +89,7 @@ TELEGRAM_CHANNEL_ID = os.getenv('ID')
 
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3', 
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
