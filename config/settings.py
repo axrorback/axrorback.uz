76d7d44335340ef96ef584213aa896fa1cc1d5a1
Ahrorjon Ibrohimjonov <axacyber1@gmail.com>
2025-12-25 22:06:15 +0500

    Add Heroku app to allowed hosts and CSRF origins

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.axrorback.uz', 'axrorback.uz','127.0.0.1','axrorback.dev']
-CSRF_TRUSTED_ORIGINS = ['https://axrorback.uz','https://www.axrorback.uz','https://axrorback.dev']
+ALLOWED_HOSTS = ['www.axrorback.uz', 'axrorback.uz','127.0.0.1','axrorback.dev','axrorback-6964a061f541.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://axrorback.uz','https://www.axrorback.uz','https://axrorback.dev','https://axrorback-6964a061f541.herokuapp.com/']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
